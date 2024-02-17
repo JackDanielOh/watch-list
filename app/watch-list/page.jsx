f815771ff1bd0a4e8d4157b323d5a63855aac07c
Jiseong Oh <66205540+JackDanielOh@users.noreply.github.com>
2024-02-17 03:06:28 +0000

    add signout

--- app/watch-list/page.jsx
+++ app/watch-list/page.jsx
@@ -1,3 +1,42 @@
+import WatchForm from "../components/WatchForm";
+
 export default function WatchList(){
-    return <h1>Watch List</h1>
+
+    const watches  = [];
+
+    retur(
+        <div className="min-h-screen bg-gray-900 text-gray-300">
+            <div className="container mx-auto p-6 sm:p-12">
+                <div className="flex justify-between items-start">
+                    <h1 className="text-5xl md:text-6xl font-extrabold text-white mb-6">
+                        My Watch List
+                    </h1>
+                    <form action="/auth/signout" method="post">
+                        <button type="submit" className="bg-gray-600 however:bg-gray-700 text-white font-bold py-2">
+                            Sign Out
+                        </button>
+                    </form>
+                </div>
+                <WatchForm />
+                <div className="mt-6">
+                    {
+                        watches.map((watch) => (
+                            <div key={watch.id} className="mb-4 p-4 bg-gray-800 rounded-lg shadow">
+                                <h2 className="text-xl text-white mb-2">{watch.brand} - {watch.name}</h2>
+                                <div className="flex space-x-2">
+                                    <form action="deleteWatch">
+                                        <input type="hidden" name="id" value={watch.id} />
+                                        <button type="submit">
+                                            Delete
+                                        </button>
+                                    </form>
+                                    <EditWatch watch={watch}/>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+    ) 
 }
