5d86a14a1fcf5975b3e0fff3a0fe2da929735eea
Jiseong Oh <66205540+JackDanielOh@users.noreply.github.com>
2024-02-17 07:06:53 +0000

    add addWatch server action

--- app/components/WatchForm.jsx
+++ app/components/WatchForm.jsx
@@ -1,3 +1,36 @@
 export default function WatchForm(){
-    
+    return (
+        <form action={addWatch} className="mb-6">
+            <div className="mb-4">
+                <label htmlFor="brand" className="block text-white mb-2">Brand</label>
+                <input 
+                    type="text" 
+                    id="brand" 
+                    name="brand" 
+                    className="shadow appearance-non border border-gray-600 bg-gray-700 rounded w-full"
+                    required />
+            </div>
+            <div>
+                <label htmlFor="model" className="block text-white mb-2">Model</label>
+                <input 
+                    type="text" 
+                    id="model" 
+                    name="model" 
+                    className="shadow appearance-non border border-gray-600 bg-gray-700 rounded w-full"
+                    required />
+            </div>
+            <div>
+                <label htmlFor="referenceNumber" className="block text-white mb-2">ReferenceNumber</label>
+                <input 
+                    type="text" 
+                    id="referenceNumber" 
+                    name="referenceNumber" 
+                    className="shadow appearance-non border border-gray-600 bg-gray-700 rounded w-full"
+                    required />
+            </div>
+            <button type="submit">
+                Add Watch
+            </button>
+        </form>
+    )
 }
